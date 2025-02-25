a71fd0c1e0b9cac236c2a1d0884ba15ed0738d5c
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-02-25 15:11:26 -0300

    Hello World

---
+++ produtosapi/src/test/java/io/github/bernardooduarte/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bernardooduarte.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
