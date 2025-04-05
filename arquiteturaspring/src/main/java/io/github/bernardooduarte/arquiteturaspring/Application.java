35bc6e1f08471e4762c4b80fa34d12a6d31b6006
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-04-05 17:47:38 -0300

    aula 23

---
+++ arquiteturaspring/src/main/java/io/github/bernardooduarte/arquiteturaspring/Application.java
@@ -0,0 +1,34 @@
+package io.github.bernardooduarte.arquiteturaspring;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Application.class, args);
+
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+
+		builder.run(args);
+
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.profiles("producao", "homologacao");
+
+		ConfigurableApplicationContext applicationContext = builder.context();
+		var produtoRepository = applicationContext.getBean("produtoRepository");
+
+		builder.profiles("producao");
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println("Nome da aplicação: " + applicationName);
+
+	}
+
+}
